08bfad4264ead687f70b0d4934107c6d9cf97bf8
PradeepaBharathi <112917638+PradeepaBharathi@users.noreply.github.com>
2024-09-25 14:45:13 +0530

    updates

--- route/cartRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { addToCart, getCart } from '../controller/cartController.js';
-const router = express.Router()
-router.post('/add', addToCart);
-router.get('/getcartproducts', getCart);
-
-export default router
